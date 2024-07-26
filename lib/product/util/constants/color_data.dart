fbf163242377bbd54c8d191f33ca57adafc4a573
Yusuf Keçer <yusufkecer0007@gmail.com>
2024-07-26 09:16:08 +0300

    Fix log error in launch_mixin.dart and update color_data.dart and string_data.dart

--- lib/product/util/constants/color_data.dart
+++ lib/product/util/constants/color_data.dart
@@ -7,13 +7,9 @@ final class ColorData {
   static const Color deepPurple = Color(0xFF4535C1);
 
   static const Color ocean = Color.fromARGB(255, 109, 175, 212);
-  //red
-  static const Color red = Color.fromARGB(255, 255, 99, 99);
-  //green
-  static const Color green = Color.fromARGB(255, 92, 189, 165);
-  //white
 
-  //amber
+  static const Color red = Color.fromARGB(255, 255, 99, 99);
+  static const Color green = Color.fromARGB(255, 92, 189, 165);
   static Color? amber = Colors.amber[700];
   static const Color white = Color(0xFFFFFFFF);
 }
